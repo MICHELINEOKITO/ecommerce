796465d6b50de971cc10edb50f09f106493c19cd
Micheline-Ok <okitobibi2@gmail.com>
2024-01-04 16:45:14 +0100

    adding navbar contents

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,27 @@
 import './Navbar.css';
+import logo from '../assets/logo.png'
+import cart from '../assets/cart-icone.png'
 
 const navbar = () =>{
 
 
     return(
-        <div>
+        <div className='navbar'>
+            <div className='nav-logo'>
+                <img src={logo} alt="" />
+                <p>SHOPPER</p>
+            </div>
+            <ul className="nav-menu">
+                <li>Shop</li>
+                <li>Men</li>
+                <li>Woman</li>
+                <li>Kids</li>
+            </ul>
+            <div className='nav-login-cart'>
+                <button>Login</button>
+                <img src={cart} alt="" />
 
+            </div>
         </div>
     )
 }
